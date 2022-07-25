9ce1d0b2622a3a12e833f2d627664167df9e7116
mltlima <miguellt@dcc.ufrj.br>
2022-07-25 16:43:21 -0300

    fix files

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-//import cors from 'cors';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import "express-async-errors";
 
@@ -9,7 +9,7 @@ import errorHandler from "./middlewares/errorHandlingMiddleware.js";
 dotenv.config();
 const app = express();
 
-//app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(router);
 app.use(errorHandler);
